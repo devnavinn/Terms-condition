4e1b766b1d834bc5a78f5072c004b04de382ab12
Soumya Moulik <97457173+Moulik98@users.noreply.github.com>
2023-03-13 10:49:24 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import animation from './components/Animation';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <animation/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Animation.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function animation() {
+  return (
+    <div>
+
+        <div>
+        WELCOME TO 21genx ACCESS
+        </div>
+        <div>
+            <div className='card'></div>
+            <div className='card'></div>
+            <div className='card'></div>
+        </div>
+    </div>
+  )
+}
+
+export default animation
